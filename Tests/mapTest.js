a5183b801598b13469d77a0c00b25f4b04fef6b5
001anj <33808621+001anj@users.noreply.github.com>
2019-10-21 00:01:12 +0200

    Create mapTest.js

---
+++ Tests/mapTest.js
@@ -0,0 +1,10 @@
+import { Selector } from 'testcafe';
+
+fixture `fixture`
+    .page `https://leafletjs.com/examples/quick-start/`;
+
+test('test', async t => {
+    await t
+        .switchToIframe('iframe')
+        .expect(Selector('path').withAttribute('stroke', 'red').getAttribute('d')).eql('M141.20355555554852,171.94704600190744a42,42 0 1,0 84,0 a42,42 0 1,0 -84,0 ');
+});
