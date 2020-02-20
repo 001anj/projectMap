868f618aa7c7839776ad7d943f2246cf6bf10bd7
001anj <33808621+001anj@users.noreply.github.com>
2020-02-20 21:13:06 +0100

    changes for test

--- Tests/mapTest.js
+++ Tests/mapTest.js
@@ -8,3 +8,5 @@ test('test', async t => {
         .switchToIframe('iframe')
         .expect(Selector('path').withAttribute('stroke', 'red').getAttribute('d')).eql('M141.20355555554852,171.94704600190744a42,42 0 1,0 84,0 a42,42 0 1,0 -84,0 ');
 });
+
+//just a test
